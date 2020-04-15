1b141f30498d6add64a57fec934ab55878c0e14c
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-04-15 22:41:33 +0430

    DisplayPictureScreen make design better

--- lib/pages/display_picture_screen.dart
+++ lib/pages/display_picture_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DisplayPictureScreen extends StatelessWidget {
@@ -13,7 +14,18 @@ class DisplayPictureScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Image.file(
+          File(imagePath),
+          fit: BoxFit.fill,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check),
+        onPressed: () {},
+      ),
     );
   }
 }
