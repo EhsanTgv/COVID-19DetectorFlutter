9a6f5bc05eba322a7558c22f2aa852fb997beb11
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-04-15 21:28:43 +0430

    TakePictureScreen Implement DisplayPictureScreen page

--- lib/pages/take_picture_screen.dart
+++ lib/pages/take_picture_screen.dart
@@ -95,3 +95,19 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     );
   }
 }
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
